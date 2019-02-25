d8bd0f690ae9b24e816453dd765ecbbc57d19d08
JiMin <you@example.com>
2019-02-25 15:08:44 +0800

    11

--- pages/release/release.js
+++ pages/release/release.js
@@ -1,66 +1,86 @@
-// pages/release/release.js
+import { promisify } from '../../utils/promise.util'
+import { $init, $digest } from '../../utils/common.util'
+
+const wxUploadFile = promisify(wx.uploadFile)
+
 Page({
+
+  data: {
+    titleCount: 0,
+    contentCount: 0,
+    title: '',
+    content: '',
+    images: []
+  },
+
+  onLoad(options) {
+    $init(this)
+  },
+
+  handleTitleInput(e) {
+    const value = e.detail.value
+    this.data.title = value
+    this.data.titleCount = value.length
+    $digest(this)
+  },
+
+  handleContentInput(e) {
+    const value = e.detail.value
+    this.data.content = value
+    this.data.contentCount = value.length
+    $digest(this)
+  },
 
   /**
    * 页面的初始数据
    */
-  data: {
-
+  chooseImage(e) {
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'],  //可选择原图或压缩后的图片
+      sourceType: ['album', 'camera'], //可选择性开放访问相册、相机
+      success: res => {
+        const images = this.data.images.concat(res.tempFilePaths)
+        // 限制最多只能留下3张照片
+        const images1 = images.length <= 3 ? images : images.slice(0, 3)
+        this.setData({
+          images: images1
+        })
+      }
+    })
+  },
+  removeImage(e) {
+    var that = this;
+    var images = that.data.images;
+    // 获取要删除的第几张图片的下标
+    const idx = e.currentTarget.dataset.idx
+    // splice  第一个参数是下表值  第二个参数是删除的数量
+    images.splice(idx, 1)
+    this.setData({
+      images: images
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleImagePreview(e) {
+    const idx = e.target.dataset.idx
+    const images = this.data.images
+    wx.previewImage({
+      current: images[idx],  //当前预览的图片
+      urls: images,  //所有要预览的图片
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //提交发布
+  submitClick: function (e) {
+    var evalList = that.data.evalList;
+    var imgList = evalList[0].imgList;
+    var imgPort = "";//图片地址，多张以逗号分割
+    if (imgList.length != 0) {
+      for (var j = 0; j < imgList.length; j++) {
+        imgPort = imgList[j] + "," + imgPort;
+      }
+    } else {
+      imgPort = "";
+    }
   }
+
 })
