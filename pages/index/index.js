e0fb96aad4f6911e2d9c7bff03db78b9bd150b9d
zhangyongkang <398868802@qq.com>
2019-02-25 16:17:36 +0800

    index

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,98 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
+
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+
+    swiperCurrent: 0,
+
+    indicatorDots: true,
+
+    autoplay: true,
+
+    interval: 3000,
+
+    duration: 800,
+
+    circular: true,
+
+    imgUrls: [
+
+      'https://p3.pstatp.com/large/43700001e49d85d3ab52',
+
+      'https://p3.pstatp.com/large/39f600038907bf3b9c96',
+
+      'https://p3.pstatp.com/large/31fa0003ed7228adf421'
+
+    ],
+
+    links: [
+
+      '../user/user',
+
+      '../user/user',
+
+      '../user/user'
+
+    ],
+    // tab
+    currentTab: 0
+
+
+
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  //滑动切换
+  swiperTab: function (e) {
+    var that = this;
+    that.setData({
+      currentTab: e.detail.current
+    });
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
+  //点击切换
+  clickTab: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+      that.setData({
+        currentTab: e.target.dataset.current
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  //轮播图的切换事件
+
+  swiperChange: function (e) {
+
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+
+      swiperCurrent: e.detail.current
+
     })
+
+  },
+
+  //点击指示点切换
+
+  chuangEvent: function (e) {
+
+    this.setData({
+
+      swiperCurrent: e.currentTarget.id
+
+    })
+
+  },
+
+  //点击图片触发事件
+
+  swipclick: function (e) {
+
+    console.log(this.data.swiperCurrent);
+
+    wx.switchTab({
+
+      url: this.data.links[this.data.swiperCurrent]
+
+    })
+
   }
+
 })
